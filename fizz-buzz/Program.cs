621d869e72e52a9b9633f793c6495c392b7a5c07
landa03 <60487799+landa03@users.noreply.github.com>
2020-02-15 19:21:56 -0700

    Add files via upload

---
+++ fizz-buzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fizz_buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int num = 1; num <= 100; num++)
+            {
+                if(num % 5 ==0 && num % 3 ==0) 
+                {
+                   Console.WriteLine("FIZZBUZZ");
+                }    
+                else if(num % 3 == 0)
+                {
+                    Console.WriteLine("FIZZ");
+                }
+                
+                else if(num % 5 == 0)
+                {
+                    Console.WriteLine("BUZZ");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
